d34765514dc12592e36be8e4fdc930bd1ba71e2d
Se-myung-Jin <tpaud94@naver.com>
2025-02-22 15:39:59 +0900

    [Common] Server Config 추가

---
+++ WebGameServer/Common/Config/ServiceConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace Common;
+
+public class MaintenanceConfig
+{
+    public string Url { get; set; }
+    public string Domain { get; set; }
+    public bool Enable { get; set; }
+};
+
+public class ServiceConfig
+{
+    public bool UsePublicAddress { get; set; }
+    public string PublicAddress { get; private set; }
+    public string PrivateAddress { get; private set; }
+    public MaintenanceConfig MaintenanceConfig { get; set; }
+
+    public async Task initializeAsync()
+    {
+        PrivateAddress = NetworkIp.GetPrivateAddress();
+        PublicAddress = UsePublicAddress ? await NetworkIp.GetPublicIpAsync() : PrivateAddress;
+    }
+
+    public string GetAddess() => PublicAddress;
+}
