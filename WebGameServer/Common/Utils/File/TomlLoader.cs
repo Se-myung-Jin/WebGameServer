8ff443f3bf131c3039360c12b2a4796a69e74241
Se-myung-Jin <tpaud94@naver.com>
2025-02-23 22:51:54 +0900

    Toml Loader 추가

---
+++ WebGameServer/Common/Utils/File/TomlLoader.cs
@@ -0,0 +1,27 @@
+﻿namespace Common;
+
+public static class TomlLoader
+{
+    public static T Load<T>(string fileName) where T : class, new()
+    {
+        var findFile = Path.GetFileName(fileName);
+        var directory = Path.GetDirectoryName(fileName);
+
+        var findPath = FileUtils.SearchFromApplicationDirectory(findFile, directory);
+        if (string.IsNullOrWhiteSpace(findPath))
+        {
+            throw new Exception($"TomlLoader File Not Found.");
+        }
+
+        try
+        {
+            return Tomlet.TomletMain.To<T>(File.ReadAllText(findPath));
+        }
+        catch (Exception ex)
+        {
+
+        }
+
+        return null;
+    }
+}
