a1d354b61cbb2176f28d272ae7f4a2d4c3ceb310
Semyung Jin <tpaud94@naver.com>
2025-02-25 14:13:02 +0900

    [Common] SleepScheduler 추가

---
+++ WebGameServer/Common/Threads/Scheduler/SleepScheduler.cs
@@ -0,0 +1,12 @@
+﻿namespace Common;
+
+public class SleepScheduler : BaseScheduler
+{
+    public override void Process(long slice)
+    {
+        if (slice > 0)
+        {
+            Thread.Sleep((int)slice);
+        }
+    }
+}
