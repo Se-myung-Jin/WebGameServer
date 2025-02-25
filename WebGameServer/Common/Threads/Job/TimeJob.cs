05e33a3b545f467dfb83eaa1fa3732d867de6b4e
Semyung Jin <tpaud94@naver.com>
2025-02-25 14:25:39 +0900

    [Common] TimeScheduler 추가
    - TimeJob
    - TimeScheduler

---
+++ WebGameServer/Common/Threads/Job/TimeJob.cs
@@ -0,0 +1,48 @@
+﻿namespace Common;
+
+public class TimeJob : Job
+{
+    public static TimeScheduler OwnerScheduler = null;
+
+    protected long _elapseMilliSecond;
+    protected long _interval;
+    protected bool _repeat;
+
+    public TimeJob(Action action, int intervalMillisecond, string description, bool repeat = true)
+            : base(action, description)
+    {
+        if (intervalMillisecond <= 0)
+        {
+            throw new Exception("TimeScheduler Zero Not Support");
+        }
+
+        _repeat = repeat;
+        _interval = intervalMillisecond * TimeSpan.TicksPerMillisecond;
+    }
+
+    public override void Start()
+    {
+        SetTime();
+
+        OwnerScheduler.Add(this);
+    }
+
+    public override void SetTime()
+    {
+        _elapseMilliSecond = DateTime.UtcNow.Ticks + _interval;
+    }
+
+    public override bool ExecutePostProcess()
+    {
+        if (_repeat)
+        {
+            SetTime();
+            
+            return true;
+        }
+
+        return false;
+    }
+
+    public override bool IsTimeOver(long timeTick) => _elapseMilliSecond <= timeTick;
+}
---
+++ WebGameServer/Common/Threads/Scheduler/TimeScheduler.cs
@@ -0,0 +1,46 @@
+﻿namespace Common;
+
+public class TimeScheduler : JobScheduler
+{
+    private ConcurrentDictionary<long, TimeJob> _timeJobById = new ConcurrentDictionary<long, TimeJob>();
+    private long _id = 0;
+
+    protected override void OnProcess(long slice)
+    {
+        var currentTick = DateTime.UtcNow.Ticks;
+        foreach (var element in _timeJobById)
+        {
+            var job = element.Value;
+            if (job.IsTimeOver(currentTick))
+            {
+                _elapseTimer.Start();
+
+                try
+                {
+                    job.Execute();
+                    if (job.ExecutePostProcess() == false)
+                    {
+                        _timeJobById.TryRemove(element.Key, out var removeItem);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+                _elapseTimer.Stop();
+                if (_elapseTimer.ElapsedMilliseconds >= 300)
+                {
+
+                }
+            }
+        }
+    }
+
+    public void Add(TimeJob job)
+    {
+        var value = Interlocked.Increment(ref _id);
+
+        _timeJobById.TryAdd(value, job);
+    }
+}
