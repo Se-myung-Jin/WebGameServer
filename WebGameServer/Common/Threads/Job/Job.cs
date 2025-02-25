3b528381433e5a956c2b1218b664ad0aa6df4c6a
Semyung Jin <tpaud94@naver.com>
2025-02-25 12:27:39 +0900

    [Common] JobScheduler 추가

---
+++ WebGameServer/Common/Threads/Job/Job.cs
@@ -0,0 +1,43 @@
+﻿namespace Common;
+
+public interface IJob
+{
+    void Start();
+    void SetTime();
+    bool IsTimeOver(long timeTick);
+    void Execute();
+}
+
+public abstract class Job : IJob
+{
+    protected Action _action = null;
+    public string Description { get; protected set; }
+
+    public Job() { }
+
+    public Job(Action action, string description)
+    {
+        _action = action;
+        Description = description;
+    }
+
+    public virtual void Start() { }
+    public virtual void SetTime() { }
+    public virtual bool IsTimeOver(long timeTick) => true;
+    public virtual void Execute()
+    {
+        try
+        {
+            _action?.Invoke();
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    public virtual bool ExecutePostProcess()
+    {
+        return true;
+    }
+}
---
+++ WebGameServer/Common/Threads/Scheduler/JobScheduler.cs
@@ -0,0 +1,40 @@
+﻿namespace Common;
+
+public class JobScheduler : PerformanceCheckScheduler
+{
+    private ConcurrentQueue<Job> _queue = new ConcurrentQueue<Job>();
+
+    protected override void OnProcess(long slice)
+    {
+        int limitJobCount = 1000;
+
+        while(_queue.TryDequeue(out var job) && limitJobCount > 0)
+        {
+            --limitJobCount;
+            _elapseTimer.Start();
+
+            try
+            {
+                job.Execute();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            _elapseTimer.Stop();
+
+            if (_elapseTimer.ElapsedMilliseconds >= 300)
+            {
+
+            }
+
+            _elapseTimer.Reset();
+        }
+    }
+
+    public void Add(Job job)
+    {
+        _queue.Enqueue(job);
+    }
+}
