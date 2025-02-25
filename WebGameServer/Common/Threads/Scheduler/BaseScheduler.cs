c246512ea40779c18a3bd8974d05580770a838d3
Semyung Jin <tpaud94@naver.com>
2025-02-25 12:09:43 +0900

    [Common] BaseScheduler 추가
    - 추후 스케줄러 기반 코드

---
+++ WebGameServer/Common/Threads/Scheduler/BaseScheduler.cs
@@ -0,0 +1,41 @@
+﻿namespace Common;
+
+public abstract class BaseScheduler
+{
+    protected int _threadId = 0;
+    protected bool _enableMultiThread = false;
+
+    public virtual void Initialize()
+    {
+
+    }
+
+    public virtual void Process(long slice)
+    {
+        if (_enableMultiThread == false)
+        {
+            if (Interlocked.CompareExchange(ref _threadId, Thread.CurrentThread.ManagedThreadId, 0) != 0)
+            {
+                return;
+            }
+        }
+
+        try
+        {
+            OnProcess(slice);
+        }
+        catch (Exception ex)
+        {
+
+        }
+
+        if (_enableMultiThread == false)
+        {
+            Interlocked.Exchange(ref _threadId, 0);
+        }
+    }
+
+    public virtual void Release() { }
+
+    protected virtual void OnProcess(long slice) { }
+}
