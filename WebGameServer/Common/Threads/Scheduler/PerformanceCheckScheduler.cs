151f3a87bb824029910701dc18417e232d63b763
Semyung Jin <tpaud94@naver.com>
2025-02-25 12:18:40 +0900

    [Common] Performance Check Scheduler 추가

---
+++ WebGameServer/Common/Threads/Scheduler/PerformanceCheckScheduler.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace Common;
+
+public class PerformanceCheckScheduler : BaseScheduler
+{
+    protected Stopwatch _elapseTimer = new Stopwatch();
+
+    public override void Process(long slice)
+    {
+        if (Interlocked.CompareExchange(ref _threadId, Thread.CurrentThread.ManagedThreadId, 0) != 0)
+        {
+            return;
+        }
+
+        try
+        {
+            OnProcess(slice);
+        }
+        catch (Exception ex)
+        {
+
+        }
+
+        _elapseTimer.Reset();
+
+        Interlocked.Exchange(ref _threadId, 0);
+    }
+}
