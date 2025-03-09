82c234ed7ce0e5f12dcc6a99a327b43588cddca5
Se-myung-Jin <tpaud94@naver.com>
2025-03-09 13:44:59 +0900

    [Game] 해당 경로 패킷 Handle 추가

---
+++ WebGameServer/GameServer/Service/Apis/Hello.cs
@@ -0,0 +1,15 @@
+﻿#pragma warning disable CS1998
+
+namespace GameServer;
+
+public partial class RestApiRouter
+{
+    [ApiHandleAttribute]
+    private async Task<IWebPacket> Hello(string clientAddress, IWebPacket protocol)
+    {
+        // var pkt = protocol as PKT_WEB_CS_HELLO;
+        // return new PKT_WEB_SC_HELLO { Response = $"{BlindSharpLib.Utils.TimeUtils.GetTime().ConvertTimeString()} Message: {pkt.Hello}" };
+
+        return null;
+    }
+}
